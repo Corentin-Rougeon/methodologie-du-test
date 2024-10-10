37946fc62581acae29b1b72ee1246b65948dbc83
Corentin-Rougeon <corentin.rougeon@ynov.com>
2024-10-10 16:14:50 +0200

    ex3

--- src/bowling_game.py
+++ src/bowling_game.py
@@ -1,6 +1,61 @@
 class Game:
+
+    def __init__(self):
+        self._frames = []
+
+        for i in range(10): self._frames.append([[], ""])
+        self._frame = 0
+        self._score = 0
+
+
     def roll(self, pins) -> None:
-        pass
+        if self._frame == 10: raise IndexError("10 frames already have done")
+        if pins > 10: raise ValueError("A roll cannot score more than 10 points")
+        if pins < 0: raise ValueError("Pins can't be a negative number")
+
+        self._frames[self._frame][0].append(pins)
+
+        if self._frame == 9:
+            if len(self._frames[self._frame][0]) == 2:
+                if pins + self._frames[self._frame][0][0] < 10:
+                    self._frame += 1
+            elif len(self._frames[self._frame][0]) == 3:
+                self._frame += 1
+        else:
+            if len(self._frames[self._frame][0]) == 2:
+                if pins + self._frames[self._frame][0][0] < 10:
+                    self._frames[self._frame][1] = "open"
+                elif pins + self._frames[self._frame][0][0] == 10:
+                    self._frames[self._frame][1] = "spare"
+                else:
+                    raise ValueError("Got more than 10 pins down in 1 frame")
+                self._frame += 1
+            else:
+                if pins == 10:
+                    self._frames[self._frame][1] = "strike"
+
+                    self._frame += 1
 
     def score(self) -> int:
-        pass
+        for i in range(9):
+            if self._frames[i][1] == "strike":
+                self._score += 10 + sum((self._frames[i + 1][0] + (self._frames[i + 2][0] if i < 8 else []))[:2])
+            elif self._frames[i][1] == "spare":
+                self._score += 10 + self._frames[i + 1][0][0]
+            else:
+                self._score += sum(self._frames[i][0])
+
+        self._score += sum(self._frames[9][0])
+
+        return self._score
+
+
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.roll(10)
+    game.roll(4)
+    game.roll(4)
+
+    print(game.score())
--- tests/test_3_bowling_game.py
+++ tests/test_3_bowling_game.py
@@ -1,2 +1,50 @@
 from src.bowling_game import Game
 import pytest
+
+@pytest.fixture
+def bowling():
+    return Game()
+
+
+
+def test_valid1(bowling):
+    bowling.roll(5)
+
+    assert bowling.score() == 5
+
+def test_valid2(bowling):
+    bowling.roll(2)
+    bowling.roll(4)
+
+    assert bowling.score() == 6
+
+def test_valid3(bowling):
+    bowling.roll(6)
+    bowling.roll(4)
+    bowling.roll(4)
+    bowling.roll(4)
+
+    assert bowling.score() == 22
+
+def test_valid4(bowling):
+    bowling.roll(10)
+    bowling.roll(4)
+    bowling.roll(4)
+    bowling.roll(4)
+
+    assert bowling.score() == 30
+
+def test_full_score(bowling):
+
+    for i in range(0,12):
+        bowling.roll(10)
+
+    assert bowling.score() == 300
+
+def test_invalid(bowling):
+    with pytest.raises(ValueError):
+        bowling.roll(11)
+
+    with pytest.raises(ValueError):
+        bowling.roll(4)
+        bowling.roll(7)
